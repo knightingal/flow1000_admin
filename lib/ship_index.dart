52eccc16e2dfc47cd1f9d5988757f7edbd0241af
knightingal <yujianming1985@hotmail.com>
2026-07-18 21:55:12 +0800

    create ShipIndexPage

---
+++ lib/ship_index.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ShipIndexPage extends StatefulWidget {
+  const ShipIndexPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return ShipIndexPageState();
+  }
+}
+
+class ShipIndexPageState extends State<ShipIndexPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.shrink();
+  }
+}
