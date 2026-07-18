047dce4abeb16396edeb6c9cedb2893187fe0602
knightingal <yujianming1985@hotmail.com>
2026-07-18 23:21:10 +0800

    implement fetchShipList

--- lib/ship_index.dart
+++ lib/ship_index.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:flow1000_admin/config.dart';
+import 'package:flow1000_admin/struct/ship.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ShipIndexPage extends StatefulWidget {
   const ShipIndexPage({super.key});
@@ -10,6 +15,17 @@ class ShipIndexPage extends StatefulWidget {
 }
 
 class ShipIndexPageState extends State<ShipIndexPage> {
+  Future<List<Ship>> fetchShipList() async {
+    final response = await http.get(Uri.parse(shipIndexUrl()));
+    if (response.statusCode == 200) {
+      List<dynamic> jsonArray = jsonDecode(response.body);
+      List<Ship> shipList = jsonArray.map((e) => Ship.fromJson(e)).toList();
+      return shipList;
+    } else {
+      throw Exception("Failed to load album");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox.shrink();
